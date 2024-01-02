57b5e07f9903cd9aa57b8633aac1fafacf282186
fatihsahin291 <musabsahin291@gmail.com>
2024-01-02 15:50:10 +0300

    Update title and add login functionality

---
+++ src/components/LoginBox.jsx
@@ -0,0 +1,52 @@
+import "../styles/LoginBox.css";
+import { useForm } from "react-hook-form";
+import { IoLogoFacebook } from "react-icons/io";
+
+function LoginBox() {
+	const { register, handleSubmit } = useForm();
+
+	return (
+		<div className="login-box">
+			<div className="login-box-bg-img"></div>
+			<form
+				className="login-inputs"
+				onSubmit={() => handleSubmit()}
+			>
+				<input
+					type="text"
+					placeholder="phone number, username, or email"
+					{...register("username")}
+				/>
+
+				<input
+					type="password"
+					placeholder="password"
+					{...register("password")}
+				/>
+
+				<button type="submit">Log In</button>
+			</form>
+
+			<div className="dividers">
+				<hr className="divider"></hr>
+				<span className="or">OR</span>
+				<hr className="divider"></hr>
+			</div>
+
+			<div className="login-with-facebook">
+				<button className="login-with-facebook-btn">
+					<IoLogoFacebook className="facebook-logo" />
+					Log In With Facebook
+				</button>
+			</div>
+
+			<div className="forgot-password">
+				<a href="https://www.instagram.com/accounts/password/reset/">
+					Forgot Password?
+				</a>
+			</div>
+		</div>
+	);
+}
+
+export default LoginBox;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,12 @@
-function login() {
-	return <div></div>;
+import "../styles/Login.css";
+import LoginBox from "../components/LoginBox";
+
+function Login() {
+	return (
+		<div className="login">
+			<LoginBox />
+		</div>
+	);
 }
 
-export default login;
+export default Login;
